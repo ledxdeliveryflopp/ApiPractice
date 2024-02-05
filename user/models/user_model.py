48af8dc7f7eb16a065003cb6c3761f85bc4e600c
LedxDeliveryFlopp <korstim18@gmail.com>
2024-02-05 21:23:53 +0700

    add user model

---
+++ user/models/user_model.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+from database.database import Base
+
+
+class UserModel(Base):
+    """Модель пользователей"""
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, unique=True, index=True)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
