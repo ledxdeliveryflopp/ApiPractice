5eb21dceab6157e47231658b5bcaad583b6c3201
LedxDeliveryFlopp <korstim18@gmail.com>
2024-02-05 21:25:15 +0700

    add async session to database.py

--- user/database/database.py
+++ user/database/database.py
@@ -2,13 +2,19 @@ import os
 from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
 database_url = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(
-    database_url, _class=AsyncSession, echo=True
+    database_url, echo=True
 )
+
+async_session = sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
+
 
 Base = declarative_base()
