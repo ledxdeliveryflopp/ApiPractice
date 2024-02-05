c684121ee8852ba8bbfec57f32572b05e470c61e
LedxDeliveryFlopp <korstim18@gmail.com>
2024-02-05 21:29:09 +0700

    create user schemas

---
+++ user/schemas/user_schemas.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, EmailStr, Field
+
+
+class UserBaseSchemas(BaseModel):
+    """Основная схема пользователя"""
+    email: EmailStr
+
+
+class UserCreateSchemas(UserBaseSchemas):
+    """Схема создания пользователя"""
+    password: str = Field(ge=6)
