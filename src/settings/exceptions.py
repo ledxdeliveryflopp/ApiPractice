6ae82e885a81689e69ab515edc70877ecc460c37
LedxDeliveryFlopp <korstim18@gmail.com>
2024-02-26 20:06:52 +0700

    return DetailedHTTPException in settings/exceptions.py

--- src/settings/exceptions.py
+++ src/settings/exceptions.py
@@ -1,16 +1,25 @@
+from typing import Any
 from fastapi import HTTPException, status
 
 
-class BadCredentials(HTTPException):
+class DetailedHTTPException(HTTPException):
+    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    detail = "Server error"
+
+    def __init__(self, **kwargs: dict[str, Any]) -> None:
+        super().__init__(status_code=self.status_code, detail=self.detail, **kwargs)
+
+
+class BadCredentials(DetailedHTTPException):
     status_code = status.HTTP_403_FORBIDDEN
     detail = "Incorrect email or password."
 
 
-class UserExist(HTTPException):
+class UserExist(DetailedHTTPException):
     status_code = status.HTTP_400_BAD_REQUEST
     detail = "User already exist."
 
 
-class UserDontExist(HTTPException):
+class UserDontExist(DetailedHTTPException):
     status_code = status.HTTP_404_NOT_FOUND
     detail = "User don't exist."
