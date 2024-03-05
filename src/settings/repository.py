8002d8a26b227ba33c6bf6927dfcb22097f9d7a7
LedxDeliveryFlopp <korstim18@gmail.com>
2024-03-05 17:57:59 +0700

    add repository for session delete/add

---
+++ src/settings/repository.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+@dataclass
+class SessionRepository:
+    """Класс для сохранения в БД"""
+    session: AsyncSession
+    object: dict
+
+    async def session_add(self):
+        """Запись объекта в БД"""
+        self.session.add(instance=self.object)
+        await self.session.commit()
+        await self.session.refresh(instance=self.object)
+
+    async def session_delete(self):
+        """Удаление объекта из БД"""
+        await self.session.delete(instance=self.object)
+        await self.session.commit()
