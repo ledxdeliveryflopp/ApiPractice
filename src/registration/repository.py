5b4665510b79d2c8dda3268046f3e2413aa36002
LedxDeliveryFlopp <korstim18@gmail.com>
2024-03-17 21:07:48 +0700

    Refactor registration repository.py.

--- src/registration/repository.py
+++ src/registration/repository.py
@@ -1,36 +1,28 @@
 from dataclasses import dataclass
-from sqlalchemy import select, or_
-from sqlalchemy.ext.asyncio import AsyncSession
 from src.registration.models import UserModel
 from src.registration.schemas import UserCreateSchemas
 from src.registration.utils import hash_password
-from src.settings.exceptions import UserExist
-from src.settings.repository import SessionRepository
-from src.vault.repository import VaultRepository
+from src.settings.service import SessionService
+from src.vault.service import VaultService
 
 
 @dataclass(repr=False, eq=False)
 class UserRepository:
     """Класс для взаимодействия с БД для пользователей"""
     user_schemas: UserCreateSchemas
-    session: AsyncSession
+    session_service: SessionService
 
     async def find_user(self):
         """Поиск пользователя по email"""
-        user = await self.session.execute(select(UserModel).filter
-                                          (or_(UserModel.email == self.user_schemas.email,
-                                               UserModel.username == self.user_schemas.username)))
-        return user.scalar()
+        user = await self.session_service.get_object_by_parameter(model=UserModel,
+                                                                  email=self.user_schemas.email)
+        return user
 
     async def create_user(self):
-        """Создание пользователя"""
-        user = await self.find_user()
-        if user:
-            raise UserExist
-        user = UserModel(username=self.user_schemas.username, email=self.user_schemas.email)
-        vault_repository = VaultRepository()
-        session_create = SessionRepository(session=self.session, object=user)
-        await session_create.session_add()
-        await vault_repository.create_secret(user_id=user.id,
-                                             password=hash_password(self.user_schemas.password))
-        return user
+        new_user = UserModel(username=self.user_schemas.username, email=self.user_schemas.email)
+        await self.session_service.create_object(save_object=new_user)
+        await self.session_service.create_object(save_object=new_user)
+        vault_service = VaultService()
+        await vault_service.create_secret(user_id=new_user.id,
+                                          password=hash_password(self.user_schemas.password))
+        return new_user
