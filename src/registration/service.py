454e9d9164bbe3005262f8de9e7337459cae813c
LedxDeliveryFlopp <korstim18@gmail.com>
2024-03-17 21:04:52 +0700

    Refactor registration service.py.

--- src/registration/service.py
+++ src/registration/service.py
@@ -7,12 +7,12 @@ class UserService:
     """Класс сервиса пользователей"""
     repository: UserRepository
 
-    async def create_user(self):
-        """Создание пользователя"""
-        new_user = await self.repository.create_user()
-        return new_user
-
     async def find_user_by_email(self):
         """Поиск пользователя по email"""
         user = await self.repository.find_user()
         return user
+
+    async def create_user(self):
+        """Создание пользователя"""
+        new_user = await self.repository.create_user()
+        return new_user
